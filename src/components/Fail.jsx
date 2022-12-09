b5f9a9ac7dc1e3e07e68d7890357cc4fe1167757
faizanshahid232 <75093651+faizanshahid232@users.noreply.github.com>
2022-12-09 17:57:43 +0400

    second changes

--- src/components/Fail.jsx
+++ src/components/Fail.jsx
@@ -1,18 +1,41 @@
 import React from 'react';
+import {
+    Button,
+    Modal,
+    ModalHeader,
+    ModalBody,
+    Row,
+    Col,
+    Container,
+  } from "reactstrap";
+  import Logo from "./images/titlelogo.png";
+import "../App.css";
 
 const Fail = () => {
     let success_failure = false;
     return (
         <>
-        <div className='bg-[#181b1d] relative flex flex-col items-center min-h-screen overflow-hidden h-full'>
-            <div className='py-4'>
-                <img className='h-[30px]' src='/egold_logo_new.png'/>
-            </div>
-            <div className='flex flex-col items-center p-[30px] bg-[#25252f] b-rad rounded-[20px] my-5 w-[400px] border-[#ff0000] border-solid border-[2px]'>
-                <h3 className='text-white font-bold text-[16px] mb-5 text-center'>Invalid Verification Code</h3>
-                <img className='h-28' src='/fail.png' />
-            </div>
+        <Container className="headcont">
+    
+    <Row md={12} style={{ background: "#0F1016"}}>
+    <Col md={12} xs={12} sm={12} style={{ marginTop: "20px", marginBottom: "20px", display: "flex", flexDirection: "column", alignItems: "center" }}>
+        <div
+          style={{ display: "flex", flexDirection: "column", alignContent: "center",  marginTop: "10px", marginLeft: "10px" }}
+        >
+          <img src={Logo} alt="" height="40px" />
         </div>
+      </Col>
+    </Row>
+    <Row>
+        <Col md={3} xs={1} sm={3}></Col>
+        <Col style={{ border: "2px solid #ff0000", marginTop: "20px", background: "#25252f",padding: "20px", borderRadius: "20px", display: "flex", flexDirection: "column", alignItems: "center" }} md={6} xs={10} sm={6}>
+        <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }} className='flex flex-col items-center p-[30px] bg-[#25252f] b-rad rounded-[20px] my-5 w-[400px] border-solid border-[2px]'>
+            <h3 className='text-white font-bold text-[16px] mb-5 text-center'>Invalid Verification Code</h3>
+            <img className='h-28' style={{ height: "112px" }} src='/fail.png' />
+        </div>
+        </Col>
+    </Row>
+    </Container>
         </>
     );
 }
